e135cd381e6756ba1a96ae902efb9529c5052269
Satoshi Matsumoto <kaorimatz@gmail.com>
2014-06-01 22:07:41 +0900

    drone: remove build-required packages

--- drone/install.sh
+++ drone/install.sh
@@ -9,6 +9,8 @@ export PATH=$GOPATH/bin:$PATH
 
 DRONE_DIR=$GOPATH/src/github.com/drone/drone
 
+yum -y install git mercurial bzr make gcc golang
+
 git clone git://github.com/drone/drone.git $DRONE_DIR
 make -C $DRONE_DIR deps
 go get github.com/GeertJohan/go.incremental
@@ -19,4 +21,6 @@ install -d /var/lib/drone
 install -m 755 $DRONE_DIR/bin/drone /usr/local/bin
 install -m 755 $DRONE_DIR/bin/droned /usr/local/bin
 
+yum -y autoremove git mercurial bzr make gcc golang
+yum clean all
 rm -rf $GOPATH
