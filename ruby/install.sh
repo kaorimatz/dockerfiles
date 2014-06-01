2115606183f85f8ef9f7c760a6b00a31f763d989
Satoshi Matsumoto <kaorimatz@gmail.com>
2014-06-01 17:06:49 +0900

    Add ruby container

---
+++ ruby/install.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -e
+set -u
+set -x
+
+yum -y install git tar gcc make openssl-devel libffi-devel readline-devel
+
+git clone git://github.com/sstephenson/ruby-build.git /ruby-build
+RUBY_CONFIGURE_OPTS='--disable-install-doc' /ruby-build/bin/ruby-build 2.1.2 /opt/ruby
+
+yum -y autoremove git tar gcc make openssl-devel libffi-devel readline-devel
+yum clean all
+rm -rf /tmp/ruby-build-*
+rm -rf /ruby-build
