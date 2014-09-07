d50a211d939e3a75f63d3469fc92adf092f288a3
Satoshi Matsumoto <kaorimatz@gmail.com>
2014-09-07 14:46:02 +0900

    Add go container

---
+++ go/README.md
@@ -0,0 +1,9 @@
+# Go
+
+## Build
+
+    docker build -t go .
+
+## Run
+
+    docker run -i -t --name=go go /bin/bash
---
+++ go/install.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+set -e
+set -u
+set -x
+
+GO_VERSION=1.3.1
+GO_SOURCE_ARCHIVE=go${GO_VERSION}.src.tar.gz
+GO_SOURCE=go
+
+yum -y install tar gcc
+
+curl https://storage.googleapis.com/golang/${GO_SOURCE_ARCHIVE} > /${GO_SOURCE_ARCHIVE}
+tar zxf /${GO_SOURCE_ARCHIVE} -C /opt
+(cd /opt/${GO_SOURCE}/src && ./make.bash)
+
+yum -y autoremove tar gcc
+yum clean all
+rm -rf /${GO_SOURCE_ARCHIVE}
