e0f9243dca25c69979c1750ef1d2ef9ce6bc3200
Satoshi Matsumoto <kaorimatz@gmail.com>
2014-11-15 23:35:23 +0900

    drone: Fix wrong path to the built binaries

--- drone/install.sh
+++ drone/install.sh
@@ -13,13 +13,11 @@ yum -y install git mercurial bzr make gcc golang
 
 git clone git://github.com/drone/drone.git $DRONE_DIR
 make -C $DRONE_DIR deps
-go get github.com/GeertJohan/go.incremental
-go get github.com/jessevdk/go-flags
 make -C $DRONE_DIR
 
 install -d /var/lib/drone
-install -m 755 $DRONE_DIR/bin/drone /usr/local/bin
-install -m 755 $DRONE_DIR/bin/droned /usr/local/bin
+install -m 755 $DRONE_DIR/packaging/root/usr/local/bin/drone /usr/local/bin
+install -m 755 $DRONE_DIR/packaging/root/usr/local/bin/droned /usr/local/bin
 
 yum -y autoremove git mercurial bzr make gcc golang
 yum clean all
