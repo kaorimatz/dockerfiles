efba14c8ac598eff54150d856d59e8b15bffd18a
Satoshi Matsumoto <kaorimatz@gmail.com>
2016-05-28 14:23:12 +0900

    erlang: update to version 18.3

--- erlang/install.sh
+++ erlang/install.sh
@@ -4,13 +4,13 @@ set -e
 set -u
 set -x
 
-ERLANG_VERSION=18.0
+ERLANG_VERSION=18.3
 ERLANG_SOURCE_ARCHIVE=otp_src_${ERLANG_VERSION}.tar.gz
 ERLANG_SOURCE=otp_src_${ERLANG_VERSION}
 
 dnf -y install tar gcc make perl ncurses-devel openssl-devel
 
-curl http://www.erlang.org/download/${ERLANG_SOURCE_ARCHIVE} > /${ERLANG_SOURCE_ARCHIVE}
+curl -L http://www.erlang.org/download/${ERLANG_SOURCE_ARCHIVE} > /${ERLANG_SOURCE_ARCHIVE}
 tar zxf /${ERLANG_SOURCE_ARCHIVE} -C /
 (cd /${ERLANG_SOURCE} && ./configure --prefix=/opt/erlang)
 make -C /${ERLANG_SOURCE} install
